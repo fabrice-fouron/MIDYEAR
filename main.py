6557c98c47209fb55ea0956f90b3a609b186f5b1
Fabrice Fouron <fouronfp05@gmail.com>
2022-01-11 22:17:55 -0500

    first commit

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from hashlib import sha256
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    return "<center><h1>Hello world</h1></center>"
+    return render_template("index.html")
 
 
 @app.route("/login")
 def login():
-    return
+    return render_template("login.html")
 
 
 @app.route("/create_account")
 def create_account():
-    return
+    return render_template("create_account")
 
 
 @app.route("/dashboard")
 def dashboard():
-    return
+    return render_template("dashboard.html")
 
 
 if __name__ == '__main__':
